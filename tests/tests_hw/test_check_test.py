aa5c6a05c9c3aa64e7556154dadfe13b4737a36b
nagores77 <elenanag1402@gmail.com>
2023-03-21 20:59:46 +0300

    Demoqa_150323

--- tests/tests_hw/test_check_test.py
+++ tests/tests_hw/test_check_test.py
@@ -10,3 +10,7 @@ def get_text(browser):
     demo_page.driver.find_element(By.CSS_SELECTOR, '#app > footer > span')
 
     assert demo_page.driver.find_element == '© 2013-2020 TOOLSQA.COM | ALL RIGHTS RESERVED.'
+
+    demo_page.btn_elements.click()
+    demo_page.driver.find_element(By.CSS_SELECTOR, '#app div.row > div.col-12.mt-4.col-md-6')
+    assert demo_page.driver.find_element == 'Please select an item from left to start practice.'
