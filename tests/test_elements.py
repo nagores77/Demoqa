cc0f86292914eef9286ed91196436d17ffdcf586
nagores77 <elenanag1402@gmail.com>
2023-03-27 21:13:28 +0300

    Demoqa_150323

--- tests/test_elements.py
+++ tests/test_elements.py
@@ -2,7 +2,6 @@ from pages.elements_page import ElementsPage
 
 
 def test_find_elements(browser):
-
     demo_page = ElementsPage(browser)
     demo_page.visit()
     assert demo_page.btns_first_menu.check_count_elements(count=9)
