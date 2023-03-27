71b10544937c61dd69e2adb0420ffac4e59567ad
nagores77 <elenanag1402@gmail.com>
2023-03-27 19:12:45 +0300

    Demoqa_150323

--- pages/base_page.py
+++ pages/base_page.py
@@ -12,6 +12,18 @@ class BasePage:
     def visit(self):
         return self.driver.get(self.base_url)
 
+    def back(self):
+        self.driver.back()
+
+
+    def forward(self):
+        self.driver.forward()
+
+
+
+    def refresh (self):
+        self.driver.refresh()
+
     # def find_element(self, locator):
     #     time.sleep(3)
     #     return self.driver.find_element(By.CSS_SELECTOR, locator)
@@ -19,9 +31,14 @@ class BasePage:
     def get_url(self):
         return self.driver.current_url
 
+
+    def get_title(self):
+        return self.driver.title
+
     def equal_url(self):
         if self.get_url() == self.base_url:
             return True
         return False
+
 
 
