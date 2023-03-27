4740f7a4ec54c7fe3b5ad2aeb5aabbdc307ddbd3
nagores77 <elenanag1402@gmail.com>
2023-03-27 21:07:46 +0300

    Demoqa_150323

--- pages/demoqa.py
+++ pages/demoqa.py
@@ -9,6 +9,8 @@ class DemoQa(BasePage):
     def __init__(self, driver):
         self.base_url = 'https://demoqa.com/'
         super().__init__(driver, self.base_url)
+        self.pageData = {'title': 'DEMOQA'}
+
         self.icon = WebElement(driver, '#app > header >a')
         self.btn_elements = WebElement(driver, '#app > div > div > div.home-body > div > div:nth-child(1)')
 
--- tests/test_seo.py
+++ tests/test_seo.py
@@ -5,4 +5,4 @@ def test_check_title_demo(browser):
 
     demo_page_1 = DemoQa(browser)
     demo_page_1.visit()
-    assert browser.title == 'DEMOQA'
+    assert browser.title == demo_page_1.pageData['title']
