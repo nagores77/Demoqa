6f7af858ee5b32befb3f600e745b7cbe7c8c3f1e
nagores77 <elenanag1402@gmail.com>
2023-03-27 11:42:15 +0300

    Demoqa_150323

--- pages/accordion.py
+++ pages/accordion.py
@@ -1,9 +1,11 @@
 from pages.base_page import BasePage
-from components.components
+from components.components import WebElement
 
 
 class Accordion(BasePage):
     def __init__(self, driver):
         self.base_url = 'https://demoqa.com/accordian'
         super().__init__(driver, self.base_url)
-        self.text_block = WebElements(driver, '#section1Content > p ')
+        self.text_block = WebElement(driver, '#section1Content > p ')
+        self.heading = WebElement(driver, '#section1Heading')
+
--- tests/tests_hw/test_visible_hw.py
+++ tests/tests_hw/test_visible_hw.py
@@ -1,7 +1,15 @@
 from pages.accordion import Accordion
+import time
 
 
 def test_visible_accordion(browser):
 
     demo_page = Accordion(browser)
     demo_page.visit()
+
+    assert demo_page.text_block.visible()
+    demo_page.heading.click()
+
+    time.sleep(2)
+
+    assert not demo_page.text_block.visible()
