0f8c030991e40ef61bc36251812c6c60e9e2be81
nagores77 <elenanag1402@gmail.com>
2023-03-27 11:00:23 +0300

    Demoqa_150323

--- tests/test_check_icon_demoqa.py
+++ tests/test_check_icon_demoqa.py
@@ -7,8 +7,8 @@ def test_icon_exist(browser):
     demo_page = DemoQa(browser)
     demo_page.visit()
     demo_page.icon.click()
-    # assert demo_page.equal_url()
-    # assert demo_page.icon()
+    assert demo_page.equal_url()
+    assert demo_page.icon.exist()
 
 
 
