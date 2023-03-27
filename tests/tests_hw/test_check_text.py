d2d1f0538581251e97188b7f89a36fc10e7faf5b
nagores77 <elenanag1402@gmail.com>
2023-03-27 10:57:11 +0300

    Demoqa_150323

--- tests/tests_hw/test_check_text.py
+++ tests/tests_hw/test_check_text.py
@@ -9,11 +9,11 @@ def get_text(browser):
 
     demo_page.visit()
 
-    assert demo_page.driver.text.get_text == '© 2013-2020 TOOLSQA.COM | ALL RIGHTS RESERVED.'
+    assert demo_page.driver.get_text == '© 2013-2020 TOOLSQA.COM | ALL RIGHTS RESERVED.'
 
     demo_page.btn_elements.click()
 
-    assert demo_page.driver.text.get_text() == 'Please select an item from left to start practice.'
+    assert demo_page.driver.get_text() == 'Please select an item from left to start practice.'
 
 
 def test_page_elements(browser):
