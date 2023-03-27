146b99c752c0a8245a0fef49532ee2dac6457266
nagores77 <elenanag1402@gmail.com>
2023-03-27 20:55:37 +0300

    Demoqa_150323

--- pages/elements_page.py
+++ pages/elements_page.py
@@ -15,6 +15,7 @@ class ElementsPage(BasePage):
         self.btn_sidebar_first = WebElement(driver, ' div:nth-child(1) > span > div')
         self.btn_sidebar_first_textbox = WebElement(driver, 'div:nth-child(1) > div > ul > #item-0 > span')
         self.btn_sidebar_first_checkbox = WebElement(driver, 'div:nth-child(1) > div > ul > #item-1 > span')
+        self.btns_first_menu = WebElement(driver, 'dib:nth-child(1) > div > ul > li')
 
     #def equal_url(self):
         #if self.get_url == self.base_url:
