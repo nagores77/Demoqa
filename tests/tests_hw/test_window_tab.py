acb56ac38d832412d23381987a816254cddc1f44
nagores77 <elenanag1402@gmail.com>
2023-04-04 21:00:04 +0300

    Demoqa_150323

--- tests/tests_hw/test_window_tab.py
+++ tests/tests_hw/test_window_tab.py
@@ -1,4 +1,4 @@
-from pages.webtables import WebTables
+from pages.links import Links
 import time
 import pytest
 import allure
@@ -7,5 +7,18 @@ import allure
 #@pytest.mark.xfail(condition=True, reason='Если страница недоступна, тест будет помечен, как xfail')
 def test_check_modal(browser):
 
-    demo_page = WebTables(browser)
+    demo_page = Links(browser)
     demo_page.visit()
+
+    assert demo_page.home_link.exist()
+    assert demo_page.home_link.get_text() == 'Home'
+    assert demo_page.home_link.get_dom_attribute('href') == 'https://demoqa.com'
+    demo_page.home_link.click()
+
+
+
+
+
+
+
+
